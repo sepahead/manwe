586a35f10b7ba5cb5dd762a292764d436176d1ff
Sepehr Mahmoudian <sepmhn@gmail.com>
2025-11-24 21:18:00 +0100

    adding rust yolo

---
+++ src/lib.rs
@@ -0,0 +1,398 @@
+// pub mod audio;
+// pub mod bs1770;
+pub mod coco_classes;
+// pub mod imagenet;
+// pub mod token_output_stream;
+// pub mod wav;
+
+use candle::utils::{cuda_is_available, metal_is_available};
+use candle::{Device, Result, Tensor};
+
+pub fn device(cpu: bool) -> Result<Device> {
+    if cpu {
+        Ok(Device::Cpu)
+    } else if cuda_is_available() {
+        Ok(Device::new_cuda(0)?)
+    } else if metal_is_available() {
+        Ok(Device::new_metal(0)?)
+    } else {
+        #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
+        {
+            println!(
+                "Running on CPU, to run on GPU(metal), build this example with `--features metal`"
+            );
+        }
+        #[cfg(not(all(target_os = "macos", target_arch = "aarch64")))]
+        {
+            println!("Running on CPU, to run on GPU, build this example with `--features cuda`");
+        }
+        Ok(Device::Cpu)
+    }
+}
+
+pub fn load_image<P: AsRef<std::path::Path>>(
+    p: P,
+    resize_longest: Option<usize>,
+) -> Result<(Tensor, usize, usize)> {
+    let img = image::io::Reader::open(p)?
+        .decode()
+        .map_err(candle::Error::wrap)?;
+    let (initial_h, initial_w) = (img.height() as usize, img.width() as usize);
+    let img = match resize_longest {
+        None => img,
+        Some(resize_longest) => {
+            let (height, width) = (img.height(), img.width());
+            let resize_longest = resize_longest as u32;
+            let (height, width) = if height < width {
+                let h = (resize_longest * height) / width;
+                (h, resize_longest)
+            } else {
+                let w = (resize_longest * width) / height;
+                (resize_longest, w)
+            };
+            img.resize_exact(width, height, image::imageops::FilterType::CatmullRom)
+        }
+    };
+    let (height, width) = (img.height() as usize, img.width() as usize);
+    let img = img.to_rgb8();
+    let data = img.into_raw();
+    let data = Tensor::from_vec(data, (height, width, 3), &Device::Cpu)?.permute((2, 0, 1))?;
+    Ok((data, initial_h, initial_w))
+}
+
+pub fn load_image_and_resize<P: AsRef<std::path::Path>>(
+    p: P,
+    width: usize,
+    height: usize,
+) -> Result<Tensor> {
+    let img = image::io::Reader::open(p)?
+        .decode()
+        .map_err(candle::Error::wrap)?
+        .resize_to_fill(
+            width as u32,
+            height as u32,
+            image::imageops::FilterType::Triangle,
+        );
+    let img = img.to_rgb8();
+    let data = img.into_raw();
+    Tensor::from_vec(data, (width, height, 3), &Device::Cpu)?.permute((2, 0, 1))
+}
+
+/// Saves an image to disk using the image crate, this expects an input with shape
+/// (c, height, width).
+pub fn save_image<P: AsRef<std::path::Path>>(img: &Tensor, p: P) -> Result<()> {
+    let p = p.as_ref();
+    let (channel, height, width) = img.dims3()?;
+    if channel != 3 {
+        candle::bail!("save_image expects an input of shape (3, height, width)")
+    }
+    let img = img.permute((1, 2, 0))?.flatten_all()?;
+    let pixels = img.to_vec1::<u8>()?;
+    let image: image::ImageBuffer<image::Rgb<u8>, Vec<u8>> =
+        match image::ImageBuffer::from_raw(width as u32, height as u32, pixels) {
+            Some(image) => image,
+            None => candle::bail!("error saving image {p:?}"),
+        };
+    image.save(p).map_err(candle::Error::wrap)?;
+    Ok(())
+}
+
+pub fn save_image_resize<P: AsRef<std::path::Path>>(
+    img: &Tensor,
+    p: P,
+    h: usize,
+    w: usize,
+) -> Result<()> {
+    let p = p.as_ref();
+    let (channel, height, width) = img.dims3()?;
+    if channel != 3 {
+        candle::bail!("save_image expects an input of shape (3, height, width)")
+    }
+    let img = img.permute((1, 2, 0))?.flatten_all()?;
+    let pixels = img.to_vec1::<u8>()?;
+    let image: image::ImageBuffer<image::Rgb<u8>, Vec<u8>> =
+        match image::ImageBuffer::from_raw(width as u32, height as u32, pixels) {
+            Some(image) => image,
+            None => candle::bail!("error saving image {p:?}"),
+        };
+    let image = image::DynamicImage::from(image);
+    let image = image.resize_to_fill(w as u32, h as u32, image::imageops::FilterType::CatmullRom);
+    image.save(p).map_err(candle::Error::wrap)?;
+    Ok(())
+}
+
+/// Loads the safetensors files for a model from the hub based on a json index file.
+pub fn hub_load_safetensors(
+    repo: &hf_hub::api::sync::ApiRepo,
+    json_file: &str,
+) -> Result<Vec<std::path::PathBuf>> {
+    let json_file = repo.get(json_file).map_err(candle::Error::wrap)?;
+    let json_file = std::fs::File::open(json_file)?;
+    let json: serde_json::Value =
+        serde_json::from_reader(&json_file).map_err(candle::Error::wrap)?;
+    let weight_map = match json.get("weight_map") {
+        None => candle::bail!("no weight map in {json_file:?}"),
+        Some(serde_json::Value::Object(map)) => map,
+        Some(_) => candle::bail!("weight map in {json_file:?} is not a map"),
+    };
+    let mut safetensors_files = std::collections::HashSet::new();
+    for value in weight_map.values() {
+        if let Some(file) = value.as_str() {
+            safetensors_files.insert(file.to_string());
+        }
+    }
+    let safetensors_files = safetensors_files
+        .iter()
+        .map(|v| repo.get(v).map_err(candle::Error::wrap))
+        .collect::<Result<Vec<_>>>()?;
+    Ok(safetensors_files)
+}
+
+// Keypoints as reported by ChatGPT :)
+// Nose
+// Left Eye
+// Right Eye
+// Left Ear
+// Right Ear
+// Left Shoulder
+// Right Shoulder
+// Left Elbow
+// Right Elbow
+// Left Wrist
+// Right Wrist
+// Left Hip
+// Right Hip
+// Left Knee
+// Right Knee
+// Left Ankle
+// Right Ankle
+pub const KP_CONNECTIONS: [(usize, usize); 16] = [
+    (0, 1),
+    (0, 2),
+    (1, 3),
+    (2, 4),
+    (5, 6),
+    (5, 11),
+    (6, 12),
+    (11, 12),
+    (5, 7),
+    (6, 8),
+    (7, 9),
+    (8, 10),
+    (11, 13),
+    (12, 14),
+    (13, 15),
+    (14, 16),
+];
+
+use candle_transformers::object_detection::{non_maximum_suppression, Bbox, KeyPoint};
+use candle::{IndexOp};
+use image::DynamicImage;
+
+pub fn report_detect(
+    pred: &Tensor,
+    img: DynamicImage,
+    w: usize,
+    h: usize,
+    confidence_threshold: f32,
+    nms_threshold: f32,
+    legend_size: u32,
+) -> Result<DynamicImage> {
+    let pred = pred.to_device(&Device::Cpu)?;
+    let (pred_size, npreds) = pred.dims2()?;
+    let nclasses = pred_size - 4;
+    // The bounding boxes grouped by (maximum) class index.
+    let mut bboxes: Vec<Vec<Bbox<Vec<KeyPoint>>>> = (0..nclasses).map(|_| vec![]).collect();
+    // Extract the bounding boxes for which confidence is above the threshold.
+    for index in 0..npreds {
+        let pred = Vec::<f32>::try_from(pred.i((.., index))?)?;
+        let confidence = *pred[4..].iter().max_by(|x, y| x.total_cmp(y)).unwrap();
+        if confidence > confidence_threshold {
+            let mut class_index = 0;
+            for i in 0..nclasses {
+                if pred[4 + i] > pred[4 + class_index] {
+                    class_index = i
+                }
+            }
+            if pred[class_index + 4] > 0. {
+                let bbox = Bbox {
+                    xmin: pred[0] - pred[2] / 2.,
+                    ymin: pred[1] - pred[3] / 2.,
+                    xmax: pred[0] + pred[2] / 2.,
+                    ymax: pred[1] + pred[3] / 2.,
+                    confidence,
+                    data: vec![],
+                };
+                bboxes[class_index].push(bbox)
+            }
+        }
+    }
+
+    non_maximum_suppression(&mut bboxes, nms_threshold);
+
+    // Annotate the original image and print boxes information.
+    let (initial_h, initial_w) = (img.height(), img.width());
+    let w_ratio = initial_w as f32 / w as f32;
+    let h_ratio = initial_h as f32 / h as f32;
+    let mut img = img.to_rgb8();
+    let font = Vec::from(include_bytes!("roboto-mono-stripped.ttf") as &[u8]);
+    let font = ab_glyph::FontRef::try_from_slice(&font).map_err(candle::Error::wrap)?;
+    for (class_index, bboxes_for_class) in bboxes.iter().enumerate() {
+        for b in bboxes_for_class.iter() {
+            println!(
+                "{}: {:?}",
+                crate::coco_classes::NAMES[class_index],
+                b
+            );
+            let xmin = (b.xmin * w_ratio) as i32;
+            let ymin = (b.ymin * h_ratio) as i32;
+            let dx = (b.xmax - b.xmin) * w_ratio;
+            let dy = (b.ymax - b.ymin) * h_ratio;
+            if dx >= 0. && dy >= 0. {
+                imageproc::drawing::draw_hollow_rect_mut(
+                    &mut img,
+                    imageproc::rect::Rect::at(xmin, ymin).of_size(dx as u32, dy as u32),
+                    image::Rgb([255, 0, 0]),
+                );
+            }
+            if legend_size > 0 {
+                imageproc::drawing::draw_filled_rect_mut(
+                    &mut img,
+                    imageproc::rect::Rect::at(xmin, ymin).of_size(dx as u32, legend_size),
+                    image::Rgb([170, 0, 0]),
+                );
+                let legend = format!(
+                    "{}   {:.0}%",
+                    crate::coco_classes::NAMES[class_index],
+                    100. * b.confidence
+                );
+                imageproc::drawing::draw_text_mut(
+                    &mut img,
+                    image::Rgb([255, 255, 255]),
+                    xmin,
+                    ymin,
+                    ab_glyph::PxScale {
+                        x: legend_size as f32 - 1.,
+                        y: legend_size as f32 - 1.,
+                    },
+                    &font,
+                    &legend,
+                )
+            }
+        }
+    }
+    Ok(DynamicImage::ImageRgb8(img))
+}
+
+pub fn report_pose(
+    pred: &Tensor,
+    img: DynamicImage,
+    w: usize,
+    h: usize,
+    confidence_threshold: f32,
+    nms_threshold: f32,
+    legend_size:u32
+) -> Result<DynamicImage> {
+    let pred = pred.to_device(&Device::Cpu)?;
+    let (pred_size, npreds) = pred.dims2()?;
+    if pred_size != 17 * 3 + 4 + 1 {
+        candle::bail!("unexpected pred-size {pred_size}");
+    }
+    let mut bboxes = vec![];
+    // Extract the bounding boxes for which confidence is above the threshold.
+    for index in 0..npreds {
+        let pred = Vec::<f32>::try_from(pred.i((.., index))?)?;
+        let confidence = pred[4];
+        if confidence > confidence_threshold {
+            let keypoints = (0..17)
+                .map(|i| KeyPoint {
+                    x: pred[3 * i + 5],
+                    y: pred[3 * i + 6],
+                    mask: pred[3 * i + 7],
+                })
+                .collect::<Vec<_>>();
+            let bbox = Bbox {
+                xmin: pred[0] - pred[2] / 2.,
+                ymin: pred[1] - pred[3] / 2.,
+                xmax: pred[0] + pred[2] / 2.,
+                ymax: pred[1] + pred[3] / 2.,
+                confidence,
+                data: keypoints,
+            };
+            bboxes.push(bbox)
+        }
+    }
+
+    let mut bboxes = vec![bboxes];
+    non_maximum_suppression(&mut bboxes, nms_threshold);
+    let bboxes = &bboxes[0];
+    let font = Vec::from(include_bytes!("roboto-mono-stripped.ttf") as &[u8]);
+    let font: ab_glyph::FontRef = ab_glyph::FontRef::try_from_slice(&font).map_err(candle::Error::wrap)?;
+
+    // Annotate the original image and print boxes information.
+    let (initial_h, initial_w) = (img.height(), img.width());
+    let w_ratio = initial_w as f32 / w as f32;
+    let h_ratio = initial_h as f32 / h as f32;
+    let mut img = img.to_rgb8();
+    for b in bboxes.iter() {
+        println!("{b:?}");
+        let xmin = (b.xmin * w_ratio) as i32;
+        let ymin = (b.ymin * h_ratio) as i32;
+        let dx = (b.xmax - b.xmin) * w_ratio;
+        let dy = (b.ymax - b.ymin) * h_ratio;
+        if dx >= 0. && dy >= 0. {
+            imageproc::drawing::draw_hollow_rect_mut(
+                &mut img,
+                imageproc::rect::Rect::at(xmin, ymin).of_size(dx as u32, dy as u32),
+                image::Rgb([255, 0, 0]),
+            );
+
+            let legend = format!(
+                "{}:{:.2}%",
+                crate::coco_classes::NAMES[0],//just input1
+                100. * b.confidence
+            );
+            imageproc::drawing::draw_text_mut(
+                &mut img,
+                image::Rgb([255, 255, 255]),
+                xmin,
+                ymin,
+                ab_glyph::PxScale {
+                    x: (legend_size as f32),
+                    y: (legend_size as f32)*2.0 - 10.,
+                },
+                &font,
+                &legend,
+            )
+
+        }
+        for kp in b.data.iter() {
+            if kp.mask < 0.6 {
+                continue;
+            }
+            let x = (kp.x * w_ratio) as i32;
+            let y = (kp.y * h_ratio) as i32;
+            imageproc::drawing::draw_filled_circle_mut(
+                &mut img,
+                (x, y),
+                2,
+                image::Rgb([0, 255, 0]),
+            );
+        }
+
+        for &(idx1, idx2) in KP_CONNECTIONS.iter() {
+            let kp1 = &b.data[idx1];
+            let kp2 = &b.data[idx2];
+            if kp1.mask < 0.6 || kp2.mask < 0.6 {
+                continue;
+            }
+            imageproc::drawing::draw_line_segment_mut(
+                &mut img,
+                (kp1.x * w_ratio, kp1.y * h_ratio),
+                (kp2.x * w_ratio, kp2.y * h_ratio),
+                image::Rgb([255, 255, 0]),
+            );
+        }
+    }
+    Ok(DynamicImage::ImageRgb8(img))
+}
